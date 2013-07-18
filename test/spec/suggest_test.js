0c455ca08176506b7f24e4c5a0c58c499ba20685
Kael <i@kael.me>
2013-07-18 15:11:35 +0800

    first commit; cortex init;

---
+++ test/spec/suggest_test.js
@@ -0,0 +1,9 @@
+describe("suggest", function(){
+    describe("suggest.my_method()", function(){
+        it("should return 1", function(done){
+            NR.use(function() {
+                
+            });
+        });
+    });
+});
